d2eea38fe66bf0caa1147db57252cb566f4cf45a
Edwin Moreno <edwinmoreno77@gmail.com>
2024-10-09 19:45:20 -0300

    branch develop added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import injectContext from "./store/appContext";
 
 function App() {
-
   return (
-     <>
+    <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1 className="text-center">hola mundo</h1>} />
+          <Route
+            path="/"
+            element={<h1 className="text-center">hola mundo</h1>}
+          />
           <Route path="*" element={<h1>Not found</h1>} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default injectContext(App);
