a2a3b92cd6f07bfa4cab105b154c6faa37abae2b
Edwin Moreno <edwinmoreno77@gmail.com>
2024-10-11 19:16:22 -0300

    folders added

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import injectContext from "./store/appContext";
 import { Register } from "./views/Register";
 
 function App() {
-
   return (
-     <>
+    <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1 className="text-center">hola mundo</h1>} />
-          <Route path="/register" element={<Register/>} />
+          <Route
+            path="/"
+            element={<h1 className="text-center">hola mundo</h1>}
+          />
+          <Route path="/register" element={<Register />} />
           <Route path="*" element={<h1>Not found</h1>} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default injectContext(App);
---
+++ src/router/PublicRoute.jsx
@@ -0,0 +1,3 @@
+export const PublicRoute = () => {
+  return <div>PublicRoute</div>;
+};
