03a8d46a5fc8fcabb6ee579b6baa08337232bbad
Edwin Moreno <edwinmoreno77@gmail.com>
2024-10-18 17:37:51 -0300

    flux.jsx added

--- src/store/flux.jsx
+++ src/store/flux.jsx
@@ -1,12 +1,42 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
-     
+      userState: {
+        userStatus: "not-authenticated", //authenticated, not-authenticated
+        user: {},
+        userErrorMessage: undefined,
+      },
     },
 
     actions: {
-     
+      onChecking: () => {
+        setStore({
+          userState: {
+            userStatus: "checking",
+            user: {},
+            userErrorMessage: undefined,
+          },
+        });
+      },
+      onLogin: (user) => {
+        setStore({
+          userState: {
+            userStatus: "authenticated",
+            user,
+            userErrorMessage: undefined,
+          },
+        });
+      },
+      onLogout: (error) => {
+        setStore({
+          userState: {
+            userStatus: "not-authenticated",
+            user: {},
+            userErrorMessage: error,
+          },
+        });
+      },
     },
   };
 };
