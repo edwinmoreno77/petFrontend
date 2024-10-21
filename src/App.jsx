c28812a5ffc472f7c188ebecb598dc68f119f20b
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-10-21 20:48:17 -0300

    login/icorrección inputs, implementación función IconEye, redireccionamiento a perfil

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import injectContext from "./store/appContext";
-<<<<<<< HEAD
 import "./global.css";
-import { Login } from "./views/Login";
-import { Register } from "./views/Register";
-import { Perfil } from "./views/Perfil";
-=======
 import { AppRouter } from "./router/AppRouter";
->>>>>>> 2e06c26da2fcfaf8dc9094c71175f759a58e11d9
 
 function App() {
   return (
