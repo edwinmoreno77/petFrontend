6faf15e836d86e4cb8098c035dde4da8dbf2fb7c
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-11-08 16:34:45 -0300

    login/revalidacion del token

--- src/router/PrivateRoutes.jsx
+++ src/router/PrivateRoutes.jsx
@@ -1,14 +1,14 @@
-import { useContext } from "react";
-import { Context } from "../store/appContext";
+// import { useContext } from "react";
+// import { Context } from "../store/appContext";
 import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export const PrivateRoutes = ({ children }) => {
-  const { store } = useContext(Context);
-  const { userStatus } = store.userState;
+  // const { store } = useContext(Context);
+  // const { userStatus } = store.userState;
 
-  return userStatus === "authenticated" ? children : <Navigate to="/login" />;
-  // return localStorage.getItem("token") ? children : <Navigate to="/login" />;
+  // return userStatus === "authenticated" ? children : <Navigate to="/login" />;
+  return localStorage.getItem("token") ? children : <Navigate to="/login" />;
 };
 
 PrivateRoutes.propTypes = {
